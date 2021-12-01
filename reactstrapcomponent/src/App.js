86a675e802e56436a1f507aa33c25318e8a0c75b
Huan <Havanhuan26111994@gmail.com>
2021-12-02 00:39:09 +0700

    Components Part 2

--- reactstrapcomponent/src/App.js
+++ reactstrapcomponent/src/App.js
@@ -3,8 +3,15 @@ import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import { Component } from 'react/cjs/react.production.min';
 import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -15,7 +22,7 @@ class App extends Component {
 
 
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     )
   }
