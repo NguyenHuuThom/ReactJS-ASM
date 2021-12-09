7b10c6a47d3b6fd19921070902b7615bb19ed714
Huan <Havanhuan26111994@gmail.com>
2021-12-09 20:06:08 +0700

    React Router

---
+++ src/components/MainComponent.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react'
+import Home from './HomeComponent';
+import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from '../components/MenuComponent';
+import Dishdetail from './DishDetail';
+import {DISHES} from '../shared/dishes';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import { Switch, Route, Redirect } from 'react-router-dom'
+
+  
+
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    }
+  }
+    
+  render() {
+    const HomePage = () => {
+      return(
+          <Home 
+          />
+      );
+    }
+    return (
+      <div>
+        <Header />
+        <Switch>
+            <Route path='/home' component={HomePage} />
+            <Route exact path='/menu' component={() =>
+            <Menu dishes={this.state.dishes} />} />
+            <Redirect to='/home' />
+          </Switch>
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default Main;
